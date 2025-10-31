7851840d697bc8b05d504629ea652e1559d78fc5
omars <omars@OMARSYED>
2025-10-31 16:57:16 +0200

    readData

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #pragma once
 using namespace std;
 
-/*-----------------------------Omar Syed -----------------------------*/
+/*-----------------------------Omar Syed-----------------------------*/
 
 void readData(string fileName,int*roverCounts,int*roverSpeed,int* checkupDurations,int &checkupNum,LinkedQueue<Request*> requestQueue) {
 	//read data from a file and store it into data structures
